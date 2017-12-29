6e93767bd32bbe59ffafbc8751a2500f8a8dbae2
taiebma <taiebma@free.fr>
2017-12-30 00:59:28 +0100

    Add comment to functions and free allocations

--- ctimer.c
+++ ctimer.c
@@ -28,6 +28,15 @@ void *_timer_handler( void *threadInfo)
   }
 }
 
+/**********
+* create_timer
+* Create a new timer
+* interval : interval to wait
+* userData : data passed to the handler function when the time ellapsed
+* sizeUserData : size of the userData
+* handler : handler function that will be called
+* return : return the id of the timer or 0 if an error occurs
+**********/
 size_t create_timer(int interval, void *userData, int sizeUserData, void (*handler)(void *))
 {
   pthread_t newThread;
@@ -44,8 +53,15 @@ size_t create_timer(int interval, void *userData, int sizeUserData, void (*handl
 
 }
 
+/**********
+* delete_timer
+* Delete the timer
+* timer : id of the timer to delete
+**********/
 void delete_timer(size_t timer)
 {
   ctimer_info *tm_info = (ctimer_info *)timer;
   pthread_cancel(tm_info->thread);
+  free(tm_info->userData);
+  free(tm_info);
 }
